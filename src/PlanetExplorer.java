7f75e4c8491048e3d2c19a01a929b3eabcca7e1e
somename <someemail>
2016-10-21 15:03:45 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -4,25 +4,35 @@
 // Finish time:
 
 public class PlanetExplorer {
-	public int getX() {
-		return x;
-	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-
 	private int x;
 	private int y;
-	
+	private String obstacles;
+	private int actualx;
+	private int actualy;
+	private String way;
+	public String getWay() {
+		return way;
+	}
+	public void setWay(String way) {
+		this.way = way;
+	}
+	public int getActualx() {
+		return actualx;
+	}
+	public void setActualx(int actualx) {
+		this.actualx = actualx;
+	}
+	public int getActualy() {
+		return actualy;
+	}
+	public void setActualy(int actualy) {
+		this.actualy = actualy;
+	}
+
+
+
+
+
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
@@ -33,6 +43,19 @@ public class PlanetExplorer {
 	 */
 		this.x=x;
 		this.y=y;
+		this.obstacles=obstacles;
+		actualx=0;
+		actualy=0;
+		way="N";
+		
+	}
+	public PlanetExplorer(int x, int y){
+		this.x=x;
+		this.y=y;
+		actualx=0;
+		actualy=0;
+		way="N";
+		
 	}
 	
 	public String executeCommand(String command){
