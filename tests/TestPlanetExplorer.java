86b034aeb3bfe9c6bd20b04b4e814448909752c3
somename <someemail>
2016-10-21 14:51:22 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -7,6 +7,6 @@ public class TestPlanetExplorer {
 	@Test
 	public void test_executeCommand() {
 		PlanetExplorer explorer = new PlanetExplorer(90,100,"(5,5)(7,8)");
-		explorer.asd();
+		assertTrue(explorer.getX()==90 && explorer.getY()==100);
 	}
 }
