cc7035c231d23f57dfd06d44a0e66ddaa501f055
somename <someemail>
2016-10-21 15:04:49 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -10,6 +10,7 @@ public class PlanetExplorer {
 	private int actualx;
 	private int actualy;
 	private String way;
+	// getters and setters
 	public String getWay() {
 		return way;
 	}
@@ -28,11 +29,24 @@ public class PlanetExplorer {
 	public void setActualy(int actualy) {
 		this.actualy = actualy;
 	}
-
-
-
-
-
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public String getObstacles() {
+		return obstacles;
+	}
+	public void setObstacles(String obstacles) {
+		this.obstacles = obstacles;
+	}
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
