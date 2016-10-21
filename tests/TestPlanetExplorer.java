c15bfabb667901cb563ef8e7924dc6ac6b3f2dc4
somename <someemail>
2016-10-21 15:04:31 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -4,9 +4,15 @@ import org.junit.Test;
 
 public class TestPlanetExplorer {
 
+	
 	@Test
 	public void test_executeCommand() {
-		PlanetExplorer explorer = new PlanetExplorer(90,100,"(5,5)(7,8)");
+		
+	}
+	@Test
+	public void test_PlanetExplorer90x100() {
+		PlanetExplorer explorer = new PlanetExplorer(90,100);
 		assertTrue(explorer.getX()==90 && explorer.getY()==100);
 	}
+	
 }
