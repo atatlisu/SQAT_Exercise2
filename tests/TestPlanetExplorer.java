282b0d360fae68d559fc1b8018c390993a3d48e4
somename <someemail>
2016-10-21 15:21:46 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -28,6 +28,15 @@ public class TestPlanetExplorer {
 		explorer.executeCommand("r");
 		assertTrue(explorer.getWay()=="E");
 	}
+	@Test
+	public void test_executeCommand3() {
+		PlanetExplorer explorer = new PlanetExplorer(90,100);
+		explorer.setActualx(7);
+		explorer.setActualy(6);
+		explorer.setWay("N");
+		explorer.executeCommand("f");
+		assertTrue(explorer.getActualx()==7 && explorer.getActualy()==7 && explorer.getWay().equals("N"));	
+	}
 	
 	
 	
